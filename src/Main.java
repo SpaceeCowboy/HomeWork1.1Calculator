084346e8d920a3ca0e71a1d3fe7fba23fd6cb40f
Alexey Shapilov <lshapilov@bk.ru>
2022-09-19 19:59:17 +0300

    check all files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(6, 2);
+
+        calc.println.accept(c);
+
+    }
+}
